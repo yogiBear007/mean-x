dd4858178008481f145dde423f3dc04604eb6e76
seb <dev.logbox@gmail.com>
2019-03-26 14:30:39 +0100

    first material components

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,13 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
+// import { AppRoutingModule } from './app-routing.module';
+import { RouterModule, Routes} from '@angular/router'
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from '@angular/material';
+
 import { ListComponent } from './components/list/list.component';
 import { CreateComponent } from './components/create/create.component';
 import { EditComponent } from './components/edit/edit.component';
 
+const routes: Routes = [
+  { path: 'create', component: CreateComponent},
+  { path: 'edit/:id', component: EditComponent},
+  { path: 'list', component: ListComponent},
+  { path: '', redirectTo: 'list', pathMatch: 'full'}
+];
 @NgModule({
   declarations: [
     AppComponent,
@@ -17,8 +26,10 @@ import { EditComponent } from './components/edit/edit.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    BrowserAnimationsModule
+    // AppRoutingModule,
+    BrowserAnimationsModule,
+    RouterModule.forRoot(routes),
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
