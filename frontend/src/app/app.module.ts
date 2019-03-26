bff5db9e8a63aae7f9ffd36152503a2ee948add3
seb <dev.logbox@gmail.com>
2019-03-26 14:05:58 +0100

    list edit create component

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,10 +4,16 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ListComponent } from './components/list/list.component';
+import { CreateComponent } from './components/create/create.component';
+import { EditComponent } from './components/edit/edit.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListComponent,
+    CreateComponent,
+    EditComponent
   ],
   imports: [
     BrowserModule,
