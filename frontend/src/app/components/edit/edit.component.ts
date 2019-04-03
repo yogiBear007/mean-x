d9991c8577005042ec3331e46e58e60819897dc1
seb <dev.logbox@gmail.com>
2019-04-03 17:36:33 +0200

    change from sass to scss

--- frontend/src/app/components/edit/edit.component.ts
+++ frontend/src/app/components/edit/edit.component.ts
@@ -4,7 +4,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-edit',
   templateUrl: './edit.component.html',
-  styleUrls: ['./edit.component.sass']
+  styleUrls: ['./edit.component.scss']
 })
 export class EditComponent implements OnInit {
 
